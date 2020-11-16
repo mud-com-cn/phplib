156451fc035d5a68c9f41e1e02bff102d37729cb
nodemud <a@akuma.com.cn>
2020-11-16 23:30:54 +0800

    new inherit

---
+++ inherit/npc.php
@@ -0,0 +1,24 @@
+<?php
+require_once(MUD_LIB.'/inherit/environment.php');
+Class Npc extends Environment {
+        function __construct() {
+        }
+        function onCommand($buf) {
+                //socket_write($this->socket,$buf,strlen($buf));
+                $GLOBALS['app']->COMMAND_D->doCommand($this,$buf);
+        }
+        function message($msg) {
+        }
+        function shortname() {
+                return HIG.$this->get('name')."(".$this->get('id').")".NOR;
+        }
+        function quit() {
+        }
+        function heart_beat() {
+                $this->message($this->shortname()." HeartBteat (".HEART_BEAT." sec)\n");
+        }
+        function setup() {
+                //$GLOBALS['app']->HEARTBEAT_D->start_heartbeat($this);
+        }
+}
+?>
---
+++ inherit/user.php
@@ -0,0 +1,30 @@
+<?php
+require_once(MUD_LIB.'/inherit/save.php');
+Class User extends Save {
+	        var $socket;
+        function __construct($socket) {
+                $this->socket = $socket;
+        }
+        function onCommand($buf) {
+                //socket_write($this->socket,$buf,strlen($buf));
+                $GLOBALS['app']->COMMAND_D->doCommand($this,$buf);
+        }
+        function message($msg) {
+                socket_write($this->socket,$msg,strlen($msg));
+        }
+        function shortname() {
+                return HIY.$this->get('name')."(".$this->get('id').")".NOR;
+        }
+        function quit() {
+                $this->save();
+                $GLOBALS['app']->HEARTBEAT_D->stop_heartbeat($this);
+                $GLOBALS['app']->SERVER_D->quit($this);
+        }
+        function heart_beat() {
+                $this->message($this->shortname()." HeartBteat (".HEART_BEAT." sec)\n");
+        }
+        function setup() {
+                $GLOBALS['app']->HEARTBEAT_D->start_heartbeat($this);
+        }
+}
+?>
