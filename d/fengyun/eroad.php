85ec39f720eb01d2053ae81f21cf555c7baaccef
nodemud <a@akuma.com.cn>
2020-11-17 10:57:02 +0800

    a

--- d/fengyun/eroad.php
+++ d/fengyun/eroad.php
@@ -3,7 +3,11 @@ require_once(MUD_LIB.'/inherit/room.php');
 Class Room_d_fengyun_eroad extends Room {
 	function __construct() {
 		$this->set("name","东大街");
-		$this->set("long","这里是东大街，这阿斯弗是否里谢谢谢谢\n啊撒旦解放了是否。");
+		$this->set("long",<<<LONG
+    这里是东大街，这阿斯弗是否里谢谢谢谢
+啊撒旦解放了是否。
+LONG
+);
 		$exits = array();
 		$exits["west"] = "/d/fengyun/center";
 		$this->set("exits",$exits);
--- d/fengyun/wroad.php
+++ d/fengyun/wroad.php
@@ -3,7 +3,10 @@ require_once(MUD_LIB.'/inherit/room.php');
 Class Room_d_fengyun_wroad extends Room {
 	function __construct() {
 		$this->set("name","西大街");
-		$this->set("long","这里是西大街，盎司附近盎司附近这阿斯弗是否里谢谢谢谢\n啊撒旦解放了是否。");
+		$this->set("long",<<<LONG
+这里是西大街，盎司附近盎司附近这阿斯弗是否里谢谢谢谢。啊撒旦解放了是否。
+LONG
+);
 		$exits = array();
 		$exits["east"] = "/d/fengyun/center";
 		$exits["northwest"] = "/d/fengyun/bookshop";
