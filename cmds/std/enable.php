383173b86e0f16967953d3452a0706cea89cfe32
nodemud <a@akuma.com.cn>
2020-11-18 15:25:11 +0800

    enable...

---
+++ cmds/std/enable.php
@@ -0,0 +1,40 @@
+<?php
+class Cmd_enable {
+	function main($user,$cmd) {
+		if(count($cmd)<3) {
+			$user->message("参数错误，应该用 enable basickskill skill\n");
+			return 1;
+		} else {
+			$basic = $cmd[1];
+			$skill = $cmd[2];
+			$ret = $user->enable_skill($basic,$skill);
+			switch($ret) {
+			case -1:
+				$user->message("没有".$basic."这种基础功法。\nenable失败。\n");
+				return 1;
+			case -2:
+				$user->message("没有".$basic."这种高级功法。\nenable失败。\n");
+				return 1;
+			case -3:
+				$user->message($basic."不是基础功法。\nenable失败。\n");
+				return 1;
+			case -4:
+				$user->message($skill."不是可以enable的高级功法。\nenable失败。\n");
+				return 1;
+			case -5:
+				$user->message($skill."不能被enable为".$basic."\nenable失败。\n");
+				return 1;
+			case -6:
+				$user->message("基础功法".$basic."等级为0\nenable失败。\n");
+				return 1;
+			case -7:
+				$user->message("高级功法".$skill."等级为0\nenable失败。\n");
+				return 1;
+			default :
+				$user->message("成功将".$skill." enable到".$basic."上。\n");
+				return 1;
+
+			}
+		}
+	}
+}
